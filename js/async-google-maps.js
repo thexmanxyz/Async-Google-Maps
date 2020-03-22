7232f583e797369cb8b71129a2c26234be55dfc8
thex <andreas.kar@gmx.at>
2020-03-22 18:17:00 +0100

    added spinner remove feature, added $.extend deep merge

--- js/async-google-maps.js
+++ js/async-google-maps.js
@@ -32,6 +32,11 @@
                 // load map
                 $container.attr('src', $container.attr('data-src'));
 
+                // remove a predefined spinner if enabled
+                if(opts.spinner.remove) {
+                    opts.removeSpinner.call($container, opts);
+                }
+
                 // callback after load initiated
                 opts.afterLoad.call($container, opts);
             }
@@ -40,7 +45,7 @@
 
     // initialize plugin and create events(s)
     $.fn.asyncGoogleMaps = function(options) {
-        var opts = $.extend({}, $.fn.asyncGoogleMaps.defaults, options);
+        var opts = $.extend(true, {}, $.fn.asyncGoogleMaps.defaults, options);
 
         // set containers containing maps
         opts.containers = this;
@@ -54,8 +59,12 @@
 
    /* default values
     *
-    * offset: Offset in pixel, a negative offset will trigger loading earlier, a postive value later.
+    * offset: Offset in pixel. A negative offset will trigger loading earlier, a postive value later.
+    * spinner.remove: Defines whether a spinner should be removed automatically after load.
+    * spinner.selector: CSS selector used to find the spinner container (starting at map parent element). 
+    * spinner.delay: Time in milliseconds waited before the spinner is removed.
     * isInViewport: Custom function to determine if container is in viewport (callback).
+    * removeSpinner: Custom function to control and define the default spinner removal behavior (callback).
     * triggerAsyncLoad: Custom function to define when the async load check is performed (callback).
     * checkAndLoad: Custom function which calls the async load and check routine (callback).
     * beforeLoad: Custom function called before the async load was initiated (callback).
@@ -64,6 +73,11 @@
     */
     $.fn.asyncGoogleMaps.defaults = {
         offset: 0,
+        spinner: {
+            remove: false,
+            selector: '.spinner-border',
+            delay: 10000
+        },
 
         // determine if container is in viewport - can be user customized
         // credits @ https://stackoverflow.com/a/33979503/2379196
@@ -79,6 +93,18 @@
 
             // detect if container is in viewport
             return containerBottom > viewportTop && containerTop + opts.offset < viewportBottom;
+        },
+
+        // remove a predefined spinner from the parent container of the map - can be user customized
+        removeSpinner: function(opts) {
+
+            // remove spinner within parent container
+            var hFunc = function() { 
+                $(this).parent().find(opts.spinner.selector).remove();
+            };
+
+            // wait a specific time in milliseconds before removing spinner
+            setTimeout(hFunc.bind(this), opts.spinner.delay);
         },
 
         // append trigger event - can be user customized
@@ -98,5 +124,5 @@
  
 })( jQuery );
 
-// attach plugin to certain Google Maps containers, basic example
+// attach plugin to Google Maps containers, basic example
 // $('.g-maps').toTopButton({});
